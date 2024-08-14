1760c94989f9974de186de68ef5bbd67c8ea302b
Geraldine Martinez <geralmartinez000@gmail.com>
2024-08-14 18:29:30 -0300

    componente

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+import MoviesName from "./components/movies/MoviesName";
 
 const watchList = [
 	{
@@ -126,32 +127,9 @@ const watchList = [
 
 function App() {
 
-  // Dada la siguiente watchlist, crear una función que tome el array original
-  // y devuelva un arreglo de objetos que solo contenga las películas:
-  // 1. Dirigidas por Cristopher Nolan.
-  // 2. Con un imdbRating > 8.0
-  // El arreglo que se devuelve SOLO DEBE CONTENER el nombre de la película, ningún otra información
-  // Es obligatorio para este CC resolverlo con la función map()
-
-  const returnMovieTitle = (watchlist) => {
-    
-    const filterDirector = watchlist.filter((list) => list.Director == 'Christopher Nolan' && list.imdbRating > '8.0').map((movie) => (
-      movie.Title
-    ))
-    console.log(filterDirector)
-    return filterDirector;
-  }
-
-  const titlesArray = returnMovieTitle(watchList)
-  console.log(titlesArray);
-
   return (
     <>
-      {
-        titlesArray.map((title) => (
-          <h1>{title}</h1>
-        ))
-      }
+     <MoviesName watchList={watchList} />
     </>
   )
 }
